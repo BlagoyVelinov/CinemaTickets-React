5c17d3ce073058f17a0218f4d10ea6b1a53d74a8
Blagoy <blagoy1234@gmail.com>
2025-08-21 13:35:01 +0300

    Add offer service

---
+++ src/services/offerService.js
@@ -0,0 +1,72 @@
+import request from "../api/request";
+
+
+const BASE_URL = "http://localhost:8080/api/offers";
+
+export default {
+    async createOffer(offerData) {
+        try {
+            const accessToken = localStorage.getItem("accessToken");
+            const result = await request.post(`${BASE_URL}/add-offer`, offerData, {
+                headers: {
+                  Authorization: `Bearer ${accessToken}`,
+                },
+              });
+            return result;
+        } catch (error) {
+            console.error('Error creating offer:', error);
+            throw error;
+        }
+    },
+
+    async deleteOffer(offerId) {
+        try {
+            const accessToken = localStorage.getItem("accessToken");
+            const result = await request.delete(`${BASE_URL}/delete-offer/${offerId}`, null, {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`,
+                },
+            });
+            return result;
+        } catch (error) {
+            console.error('Error deleting offer:', error);
+            throw error;
+        }
+    },
+
+    async editOffer(offerId, offerData) {
+        try {
+            const accessToken = localStorage.getItem("accessToken");
+            const result = await request.put(`${BASE_URL}/update-offer/${offerId}`, offerData, {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`,
+                },
+            });
+            return result;
+        } catch (error) {
+            console.error('Error editing the offer:', error);
+            throw error;
+        }
+    },
+
+    async getAllOffers() {
+        try {
+            const result = await request.get(`${BASE_URL}`);
+            const offers = Object.values(result);
+            return offers;
+        } catch (error) {
+            console.error('Error fetching all offers:', error);
+            throw error;
+        }
+    },
+
+    async getOfferById(id) {
+        try {
+            const result = await request.get(`${BASE_URL}/offer/${id}`);
+            return result;
+        } catch (error) {
+            console.error(`Error fetching offer with id ${id}:`, error);
+            throw error;
+        }
+    },
+}
