1da2b713021ef9a0056235157e52ee89831ce4c4
Blagoy <blagoy1234@gmail.com>
2025-08-22 15:36:40 +0300

    Fix all offers view

--- src/components/offers/Offers.jsx
+++ src/components/offers/Offers.jsx
@@ -1,4 +1,21 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router";
+
+import { useOffers } from "../../providers/OfferProvider";
+import styles from "./Offers.module.css";
+
 export default function OffersTab() {
+    const { allOffers, loadAllOffers, refreshAllOffers } = useOffers();
+    const [selectedOffer, setSelectedOffer] = useState(null);
+
+    useEffect(() => {
+        loadAllOffers();
+    }, [loadAllOffers]);
+
+    const cinemaOffers = allOffers.cinemaOffers || [];
+    const schoolOffers = allOffers.schoolOffers || [];
+    const businessOffers = allOffers.businessOffers || [];
+    
     return (
         <div id="content-offers" className="content-section">
             <div className="line-hor"></div>
@@ -7,18 +24,20 @@ export default function OffersTab() {
                     <div className="border-left">
                         <div className="inner">
                         
-                            <div className="admin-only">
-                                <a href="/offers/add-offer" className="admin-btn admin-btn-add">
-                                <i className='bx bx-plus'></i>
-                                Add Offer
-                                </a>
-                            </div>
-                        
                             <div className="container-fluid static">
                                 <div className="row mb-md">
                                     <div className="col-md-12">
                                         <h3>Cinema <span>Offers</span></h3>
-                                        <img src="./images/company-vouchers.jpg" className="img-responsive visible-xs" alt="offer-img"/>
+                                        <div className={styles.offerContainer}>
+                                            {cinemaOffers.map(offer => (
+                                                <Link key={offer.id} className={styles.offerItem}>
+                                                    <img src={offer.imageUrl} className={styles.offerImg} alt="offer-img"/>
+                                                    <h4 className={styles.offerTitle}>{offer.title}</h4>
+                                                    {/* <div className={styles.offerDescription}>{offer.description}</div> */}
+                                                </Link>
+                                            ))}
+                                        </div>
+                                       
                                     </div>
                                     <div className="col-md-3 offer-card" data-category="cinema">
                                     </div>
@@ -27,21 +46,39 @@ export default function OffersTab() {
                                 <div className="row mb-md">
                                     <div className="col-md-12">
                                         <h3>For the <span>Schools</span></h3>
-                                        <img src="./images/company-vouchers.jpg" className="img-responsive visible-xs" alt="offer-img"/>
+                                        <div className={styles.offerContainer}>
+                                            {schoolOffers.map(offer => (
+                                                <Link key={offer.id} className={styles.offerItem}>
+                                                    <img src={offer.imageUrl} className={styles.offerImg} alt="offer-img"/>
+                                                    <h4 className={styles.offerTitle}>{offer.title}</h4>
+                                                    {/* <div className="offerDescription">{offer.description}</div> */}
+                                                </Link>
+                                            ))}
+                                        </div>
                                     </div>
                                 </div>
                                 <div className="col-md-3 offer-card" data-category="school">
                                 </div>
+
+                                <div className="row mb-md">
+                                    <div className="col-md-12">
+                                        <h3>For the <span>Business</span></h3>
+                                        <div className={styles.offerContainer}>
+                                            {businessOffers.map(offer => (
+                                                <Link key={offer.id} className={styles.offerItem}>
+                                                    <img src={offer.imageUrl} className={styles.offerImg} alt="offer-img"/>
+                                                    <h4 className={styles.offerTitle}>{offer.title}</h4>
+                                                    {/* <div className="offerDescription">{offer.description}</div> */}
+                                                </Link>
+                                            ))}
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-md-3 offer-card" data-category="business">
+                                </div>
+
                             </div>
 
-                            <div className="row mb-md">
-                                <div className="col-md-12">
-                                    <h3>For the <span>Business</span></h3>
-                                    <img src="./images/company-vouchers.jpg" className="img-responsive visible-xs" alt="offer-img"/>
-                                </div>
-                            </div>
-                            <div className="col-md-3 offer-card" data-category="business">
-                            </div>
                         </div>
                     </div>
                 </div>
