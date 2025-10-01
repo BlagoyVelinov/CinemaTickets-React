52aaaa8e4c06b2b090641353faff22f5b86502d7
Blagoy <blagoy1234@gmail.com>
2025-10-01 10:15:13 +0300

    Fix displayng mobile size

--- src/components/header/nav-links/HeaderNav.jsx
+++ src/components/header/nav-links/HeaderNav.jsx
@@ -4,24 +4,24 @@ import { Link, NavLink } from "react-router";
 export default function HeaderNav() {
     const [showDropdown, setShowDropdown] = useState(false);
     
-    const dropdownMenu = (e) => {
-        setShowDropdown(false);
+    const toggleMenu = () => {
+        setShowDropdown((prev) => !prev);
     }
     
     return (
         <div className="row-2" data-show-tabs="true">
-            <ul>
+            <ul className={showDropdown ? "mobile-nav-active" : ""}>
                 <li>
-                    <NavLink to="/" style={({ isActive }) => isActive ? {color: 'green'} : {}}
+                    <NavLink to="/" onClick={toggleMenu} style={({ isActive }) => isActive ? {color: 'green'} : {}}
                     >
                         Home
                     </NavLink></li>
-                <li><NavLink to="/program">Program</NavLink></li>
-                <li><NavLink to="/offers">Offers</NavLink></li>
-                <li><NavLink to="/4dx">4-DX</NavLink></li>
-                <li className="last"><NavLink to="/imax">IMAX</NavLink></li>
+                <li><NavLink to="/program" onClick={toggleMenu}>Program</NavLink></li>
+                <li><NavLink to="/offers" onClick={toggleMenu}>Offers</NavLink></li>
+                <li><NavLink to="/4dx" onClick={toggleMenu}>4-DX</NavLink></li>
+                <li className="last"><NavLink to="/imax" onClick={toggleMenu}>IMAX</NavLink></li>
             </ul>
-            <Link className="icon-menu" onClick={dropdownMenu}>
+            <Link className="icon-menu" onClick={toggleMenu}>
                 <i className="fa fa-bars"></i>
             </Link>
         </div>
